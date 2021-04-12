edd0247e987d38ed4d9134773c94b5dd4fc7fe7a
Your Name <you@example.com>
2021-04-12 15:51:58 +0600

    added PostList and Post Full components in component Post

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,6 +1,14 @@
-const Posts = () => {
+import { Route } from "react-router"
+import PostFull from "./PostFull/PostFull";
+import PostList from "./PostList/PostList"
+
+const Posts = ({ match}) => {
     return ( 
-    <div></div>
+    <div>
+        {match.isExact}
+        <PostList />
+        <Route path="/posts/:id" component={PostFull} />
+    </div>
      );
 }
  
